2986c283db3a2ee8914392b6cf97a7b6b66557ac
InJeckt <p1o1l1i1e1x1o1v@mail.ru>
2024-07-23 22:57:09 +0500

    added byte addressing output in interpreter

--- interpreter/VS/interpreter logisim evolution/interpreter logisim evolution/output.cpp
+++ interpreter/VS/interpreter logisim evolution/interpreter logisim evolution/output.cpp
@@ -1,23 +1,32 @@
 #include "output.h"
 
-output_t::output_t(std::string a_file_path)
+output_t::output_t(std::string a_file_path, bool a_is_word_addressing)
 	: m_file(a_file_path),
 	m_word_count(0),
-	m_max_word_count(1 << bits_in_address)
+	m_max_word_count(1 << bits_in_address),
+	m_is_word_addressing(a_is_word_addressing)
 {
 	if (!m_file.is_open()) {
 		error("Output file wasn't open");
 	}
 	m_file << "v3.0 hex words addressed";
+	if (m_is_word_addressing) {
+		addresses_in_line = addresses_in_line_word_addressing;
+		addresses_by_instr = addresses_by_instr_word_addressing;
+	}
+	else {
+		addresses_in_line = addresses_in_line_byte_addressing;
+		addresses_by_instr = addresses_by_instr_byte_addressing;
+	}
 }
 
 output_t::~output_t()
 {
 }
 
-void output_t::add_word(std::string a_word)
+void output_t::add_word(std::string a_instr)
 {
-	if (m_word_count % 8 == 0) {
+	if (m_word_count % addresses_in_line == 0) {
 		std::string tmp_string;
 		std::stringstream hex_word_count;
 		hex_word_count << std::hex << m_word_count;
@@ -27,12 +36,18 @@ void output_t::add_word(std::string a_word)
 		m_file << '\n' << tmp_string << std::hex << hex_word_count.str() << ':';
 	}
 	m_file << ' ';
-	for (size_t i = 0; i < 8 - a_word.length(); i++)
+	for (size_t i = 0; i < instr_length - a_instr.length(); i++)
 	{
 		m_file << '0';
 	}
-	m_file << a_word;
-	m_word_count++;
+	if (m_is_word_addressing) {
+		m_file << a_instr;
+	}
+	else {
+		m_file << a_instr.substr(instr_length - 2, 2) << ' ' << a_instr.substr(instr_length - 4, 2) << ' ' <<
+			a_instr.substr(instr_length - 6, 2) << ' ' << a_instr.substr(instr_length - 8, 2);
+	}
+	m_word_count += addresses_by_instr;
 }
 
 void output_t::finish_file()
@@ -41,4 +56,14 @@ void output_t::finish_file()
 		add_word("00000000");
 	}
 	m_file.close();
+}
+
+void output_t::add_word_to_word_address(std::string a_word)
+{
+
+}
+
+void output_t::add_word_to_byte_address(std::string a_word)
+{
+
 }
--- interpreter/VS/interpreter logisim evolution/interpreter logisim evolution/output.h
+++ interpreter/VS/interpreter logisim evolution/interpreter logisim evolution/output.h
@@ -10,7 +10,7 @@ extern void error(std::string msg);
 class output_t
 {
 public:
-	output_t(std::string a_file_path);
+	output_t(std::string a_file_path, bool a_is_word_addressing = false);
 	~output_t();
 
 	void add_word(std::string a_word);
@@ -20,10 +20,20 @@ private:
 	size_t m_word_count;
 	const size_t m_max_word_count;
 	std::ofstream m_file;
+	bool m_is_word_addressing;
+	size_t addresses_in_line;
+	size_t addresses_by_instr;
 
 	enum output_stats_t {
-		words_in_line = 8,
-		bits_in_address = 16
+		bits_in_address = 16,
+		instr_length = 8,
+		addresses_in_line_word_addressing = 8,
+		addresses_in_line_byte_addressing = 16,
+		addresses_by_instr_word_addressing = 1,
+		addresses_by_instr_byte_addressing = 4,
 	};
+
+	void add_word_to_word_address(std::string a_word);
+	void add_word_to_byte_address(std::string a_word);
 };
 
